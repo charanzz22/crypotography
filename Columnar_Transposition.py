33f81c70a579652025c2672b50203a935e440dab
charanzz22 <gbandaru2@student.gitam.edu>
2026-09-08 20:33:13 +0530

    Update and rename Double_Columnar_Transposition.c to Columnar_Transposition.py

---
+++ Columnar_Transposition.py
@@ -0,0 +1,19 @@
+
+text = input("Enter text: ").replace(" ", "").upper()
+key = [3, 1, 4, 2]
+while len(text) % 4 != 0:
+    text += "X"
+encrypted = ""
+for n in range(1, 5):
+    col = key.index(n)
+    for i in range(col, len(text), 4):
+        encrypted += text[i]
+print("Encrypted:", encrypted)
+decrypted = [""] * len(text)
+k = 0
+for n in range(1, 5):
+    col = key.index(n)
+    for i in range(col, len(text), 4):
+        decrypted[i] = encrypted[k]
+        k += 1
+print("Decrypted:", "".join(decrypted))
